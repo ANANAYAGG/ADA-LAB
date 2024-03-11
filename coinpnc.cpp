0bf680a1749e865c149519e8a80d70316d63c543
ANANAYAGG <121540571+ANANAYAGG@users.noreply.github.com>
2024-03-11 19:39:49 +0530

    Update coinpnc.cpp

--- coinpnc.cpp
+++ coinpnc.cpp
@@ -4,28 +4,42 @@
 using namespace std;
 using namespace std::chrono;
 
-void towerOfHanoi(int n, char source, char auxiliary, char destination) {
-    if (n >= 1) {
-    towerOfHanoi(n - 1, source, destination, auxiliary);
-    towerOfHanoi(n - 1, auxiliary, source, destination);
+void coins(int arr[], int k, int n)
+{
+    if(k == n + 1)
+    {
+        return;
+    }
+
+    arr[k] = 1;
+    coins(arr, k + 1, n);
+
+    arr[k] = 0;
+    coins(arr, k + 1, n);
 }
-}
 
-int main() {
-    int *arr = new int[10000];  // Dynamic array
+int main()
+{
+    for (int i = 10; i <= 25; i += 3)
+    {
+        int *arr = new int[i]; // Dynamic array for each iteration
 
-    for (int i = 15; i <= 30; i += 1) {
-        auto start = high_resolution_clock::now();  // Time count
+        auto start = high_resolution_clock::now(); // Time count
 
-        for (int m = 1; m <= 10; m++) {
-            towerOfHanoi(i, 'A', 'B', 'C');  
+        for (int m = 1; m <= 10; m++)
+        {
+            coins(arr, 0, i); 
         }
 
         auto stop = high_resolution_clock::now();
 
         auto duration = duration_cast<nanoseconds>(stop - start) / 10;
+        cout << "Inputs: " << i << "\tTime taken: " << duration.count() << " nanoseconds.." << endl;
+        // cout<<i<<endl;
+        //cout<<duration.count()<<endl;
 
-      cout << "time taken" <<duration.count() << endl;
+        delete[] arr;
     }
+
     return 0;
 }
